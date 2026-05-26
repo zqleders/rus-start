5f24adfbfee5ebe10302d52abe0e6772c51a79b2
zqleders <zqleaders@gmail.com>
2026-05-26 11:52:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def run():
     login_url = os.environ.get("LOGIN_URL")
     
     # 设定坐标
-    CLICK_X = 1427
-    CLICK_Y = 173
+    CLICK_X = 1370
+    CLICK_Y = 100
     
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
